a6cce26eff7383397e930b900e050fe57aaca792
Nolan Leggeri <nolanpubi@gmail.com>
2023-04-13 13:48:53 +0200

    insh

--- srcs/main.c
+++ srcs/main.c
@@ -27,8 +27,8 @@ int	main(int argc, char **argv, char **envp)
 			list = NULL;
 			rl = readline("minishell-1.0$ ");
 			list = split_new_format(rl, list);
-			list = replace_if_dollar(list, envp);
-			while (list)
+			/*list = replace_if_dollar(list, envp);
+			*/while (list)
 			{
 				printf("LISTE %s\n", list->str);
 				list = list->next;
