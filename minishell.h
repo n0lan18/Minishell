b6a49c82fdb1cc8613f63121bf6a851703f50483
Sylvain Nicolet <nicoletsylvain@gmail.com>
2023-04-17 18:07:18 +0200

    Fix to merge

--- minishell.h
+++ minishell.h
@@ -15,8 +15,10 @@
 
 # include <stdlib.h>
 # include <stdio.h>
+# include <unistd.h>
 # include <readline/readline.h>
 # include <readline/history.h>
+# include <signal.h>
 # include "./libft/libft.h"
 
 typedef struct s_param
@@ -37,51 +39,60 @@ typedef struct s_list
 	struct s_list	*next;
 }	t_list;
 
-/*************Readline_to_tab***************/
-int		search_in_str(char s, char *str);
-int		count_word(const char *s, char c, int deb, int end);
-int		count_word_fort_second_split(const char *s);
-int		count_word_fort_second_split_bis(const char *s, int num, int i);
-char	**ft_split_next_gen(char const *s, char c, int deb, int end);
-char	**ft_split_in_ft_split(char const *s);
-char	**ft_split_in_ft_split_bis(char const *s, t_param a);
-char	*ft_strcpy_new(char *dst, char *str, int deb, int end);
-t_list	*split_new_format(char *rline, t_list *list);
-int		remove_space_begin_or_end(char *str, int pos);
+/************* Readline_to_tab ***************/
+int			search_in_str(char s, char *str);
+int			count_word(const char *s, char c, int deb, int end);
+int			count_word_fort_second_split(const char *s);
+int			count_word_fort_second_split_bis(const char *s, int num, int i);
+char		**ft_split_next_gen(char const *s, char c, int deb, int end);
+char		**ft_split_in_ft_split(char const *s);
+char		**ft_split_in_ft_split_bis(char const *s, t_param a);
+char		*ft_strcpy_new(char *dst, char *str, int deb, int end);
+t_list		*split_new_format(char *rline, t_list *list);
+int			remove_space_begin_or_end(char *str, int pos);
 
-/*************Check_dollar_in_list********/
-t_list	*replace_dollar_if_first(t_list *list, char **env);
-t_list	*replace_dollar_if_after(t_list *list, t_list *tmp, char **env);
-t_list	*replace_dollar_if_after_bis(t_list *temp, t_list *tmp, char **env);
-t_list	*replace_if_dollar(t_list *list, char **env);
+/************* Check_dollar_in_list ********/
+t_list		*replace_dollar_if_first(t_list *list, char **env);
+t_list		*replace_dollar_if_after(t_list *list, t_list *tmp, char **env);
+t_list		*replace_dollar_if_after_bis(t_list *temp, t_list *tmp, char **env);
+t_list		*replace_if_dollar(t_list *list, char **env);
 
-/*************Check_dollar_in_list_bis****/
-int	search_dollar_in_list(t_list *list);
-char	*add_var_and_word(char *str, char *str1, char *tab);
-char	*search_var_in_env(char *str, char **env);
-int	compare_length_in_env(char *env);
+/************* Check_dollar_in_list_bis ****/
+int			search_dollar_in_list(t_list *list);
+char		*add_var_and_word(char *str, char *str1, char *tab);
+char		*search_var_in_env(char *str, char **env);
+int			compare_length_in_env(char *env);
 
-/*************check_words_in_tab***********/
-int		check_which_type(char **tab, char **env);
+/************* check_words_in_tab ***********/
+int			check_which_type(char **tab, char **env);
 
-/*************search_cmd*******************/
-int		search_path_in_env(char **envp);
-char	*join_all_path(char *env, char *cmd, char slash);
-char	*existence_of_cmd(char **env, char *cmd);
-int		search_of_type_cmd(char **env, char *tab);
+/************* search_cmd *******************/
+int			search_path_in_env(char **envp);
+char		*join_all_path(char *env, char *cmd, char slash);
+char		*existence_of_cmd(char **env, char *cmd);
+int			search_of_type_cmd(char **env, char *tab);
 
-/*************free_fonctions***************/
-void	free_double_tab(char **tab);
+/************* free_fonctions ***************/
+void		free_double_tab(char **tab);
 
-/*************Fonctions_list***************/
-t_list	*add_new_element(t_list *list, char *tab);
-t_list	*char_to_tab(char *tab, t_list *list);
-void	free_list(t_list *a);
-int	search_case_in_list(t_list *list, char *str);
-int		size_list(t_list *list);
+/************* Fonctions_list ***************/
+t_list		*add_new_element(t_list *list, char *tab);
+t_list		*char_to_tab(char *tab, t_list *list);
+void		free_list(t_list *a);
+int			search_case_in_list(t_list *list, char *str);
+int			size_list(t_list *list);
 
-/*************Fonctions_list_bis***********/
-t_list	*remove_first_elem_if_var_NULL(t_list *list);
-t_list	*remove_after_first_elem_list_if_NULL(t_list *list);
+/************* Fonctions_list_bis ***********/
+t_list		*remove_first_elem_if_var_NULL(t_list *list);
+t_list		*remove_after_first_elem_list_if_NULL(t_list *list);
+
+/************* Signals ***********/
+void		ft_init_signals(void);
+
+/************* Closing functions ***********/
+void		ft_close(void);
+
+/************* External functions ***********/
+extern void	rl_replace_line(const char *c, int i);
 
 #endif
--- srcs/main.c
+++ srcs/main.c
@@ -12,43 +12,27 @@
 
 #include "../minishell.h"
 
-//int	main(int argc, char **argv, char **envp)
-//{
-//	char	*rl;
-//	t_list	*list;
-//
-//	(void) envp;
-//	if (argc == 1)
-//	{
-//		list = NULL;
-//		rl = "0";
-//		while (ft_strncmp(rl, "exit", ft_strlen(rl)) != 0)
-//		{
-//			list = NULL;
-//			rl = readline("minishell-1.0$ ");
-//			list = split_new_format(rl, list);
-//			list = replace_if_dollar(list, envp);
-//			while (list)
-//			{
-//				printf("LISTE %s\n", list->str);
-//				list = list->next;
-//			}
-//		}
-//		return (0);
-//	}
-//	perror(argv[1]);
-//	return (0);
-//}
-
-int	main(void)
+int	main(int argc, char **argv, char **envp)
 {
 	char	*prompt_output;
+	t_list	*list;
 
+	(void)argc;
+	(void)argv;∆
+	list = NULL;
 	prompt_output = "";
 	while (prompt_output != NULL)
 	{
 		ft_init_signals();
 		prompt_output = readline("minishell-1.0$ ");
+		list = split_new_format(prompt_output, list);
+		list = replace_if_dollar(list, envp);
+		while (list)
+		{
+			printf("LISTE %s\n", list->str);
+			list = list->next;
+		}
+		free_list(list);
 		add_history(prompt_output);
 	}
 	ft_close();
