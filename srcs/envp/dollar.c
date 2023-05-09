1a10f976387c0d80f531bafd61c62115007daf04
Sylvain Nicolet <nicoletsylvain@gmail.com>
2023-05-10 00:15:17 +0200

    Backup before refactoring

--- srcs/envp/dollar.c
+++ srcs/envp/dollar.c
@@ -19,14 +19,29 @@ char	*ft_getenv(t_envp *envp, char *name)
 	current = envp;
 	while (current)
 	{
-		if (ft_strncmp(current->name, name, ft_strlen(name)) == 0)
+		if (ft_strncmp(current->name, name, ft_strlen(name) + 1) == 0)
 			return (current->value);
 		current = current->next;
 	}
 	return (NULL);
 }
 
-void	ft_replace_dollar(t_env *env, t_token *current)
+void	ft_replace_dollar_correct_value(t_envp *envp, t_dollar **list)
+{
+	t_dollar	*current;
+
+	current = *list;
+	while (current)
+	{
+		if (current->str[0] == '$')
+		{
+			current->str = ft_getenv(envp, ft_strtrim(current->str, "$"));
+		}
+		current = current->next;
+	}
+}
+
+static void	ft_replace_dollar(t_env *env, t_token *current)
 {
 	char		**tab_dollars;
 	t_dollar	*list_dollars;
@@ -35,6 +50,7 @@ void	ft_replace_dollar(t_env *env, t_token *current)
 	list_dollars = NULL;
 	tab_dollars = ft_split_dollar(ft_strtrim(current->str, "\""));
 	ft_create_list_dollars(&list_dollars, tab_dollars);
+	ft_replace_dollar_correct_value(env->envp, &list_dollars);
 	db_print_dollar(list_dollars);
 }
 
