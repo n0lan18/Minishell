8eb806e20233dc32a33f5396977b232d384c042d
Nolan Leggeri <nolanpubi@gmail.com>
2023-05-12 11:34:05 +0200

    create and finish exec cmd

--- srcs/builtins/launch_cmd.c
+++ srcs/builtins/launch_cmd.c
@@ -6,118 +6,26 @@
 /*   By: nleggeri <nleggeri@42.student.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/11 14:24:44 by nleggeri          #+#    #+#             */
-/*   Updated: 2023/05/11 19:33:54 by nleggeri         ###   ########.fr       */
+/*   Updated: 2023/05/12 11:33:28 by nleggeri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../minishell.h"
-/**
- * This fonction search in the list if the first element is a cmd or not
- * @param env
- * 
- * @return int
- */
-int	check_if_cmd_first(t_env env)
-{
-	if (env.token->type == E_CMD)
-		return (1);
-	return (0);
-}
 
 /**
- * Calcule how many size is the char **tab with E_CMD and E_SPACE and E_STRING
- * @param token
+ * This fonction launch the cmd if there is a file in the list. The i variable
+ * is a counter for the execve fonction and not created here for the norm.
+ * @param tmp 
+ * @param path 
+ * @param cmd 
+ * @param i
  * 
- * @return int
+ * @return void
  */
-static	int	size_of_char_for_cmd_options(t_token *token)
-{
-	t_token	*tmp;
-	int		i;
-
-	i = 0;
-	tmp = token;
-	while (tmp)
-	{
-		if (tmp->type == E_CMD || tmp->str[0] == '-' || tmp->type == E_SPACE)
-		{
-			if (tmp->type != E_SPACE)
-				i++;
-			tmp = tmp->next;
-		}
-		else
-			break ;
-	}
-	return (i);
-}
-
-/**
- * This fonction create an array with cmd and options for execve
- * @param token
- * 
- * @return char**
- */
-static	char	**tab_with_cmd_and_options(t_token *token)
-{
-	t_token	*tmp;
-	char	**tab;
-	int		i;
-
-	i = 0;
-	tab = malloc(sizeof(char *) * (size_of_char_for_cmd_options(token) + 1));
-	if (!tab)
-		return (NULL);
-	tmp = token;
-	while (i < size_of_char_for_cmd_options(token))
-	{
-		if (tmp->type == E_CMD || tmp->str[0] == '-')
-			tab[i++] = ft_strdup(tmp->str);
-		tmp = tmp->next;
-	}
-	tab[i] = NULL;
-	return (tab);
-}
-
-static	char	**ft_create_path_for_execve(t_envp *env, char *cmd)
-{
-	t_envp	*tmp;
-	char	*env_path;
-	char	**envp;
-	int		i;
-
-	i = 0;
-	tmp = env;
-	while (tmp && ft_strncmp(tmp->name, "PATH", 4) != 0)
-		tmp = tmp->next;
-	env_path = ft_strdup(tmp->value);
-	envp = ft_split(env_path, ':');
-	while (envp[i])
-	{
-		envp[i] = join_all_path(envp[i], cmd, '/');
-		i++;
-	}
-	return (envp);
-}
-
-static	int	search_e_file_in_list(t_token *token)
-{
-	t_token	*tmp;
-
-	tmp = token;
-	while (tmp)
-	{
-		if (tmp->type == E_FILE)
-			return (1);
-		tmp = tmp->next;
-	}
-	return (0);
-}
-
-void	launch_cmd_if_file_in_list(t_token *tmp, char **path, char **cmd)
+void	launch_cmd_if_file_in_list(t_token *tmp, char **path, char **cmd, int i)
 {
 	int		status;
 	pid_t	pid;
-	int		i;
 
 	while (tmp)
 	{
@@ -143,11 +51,19 @@ void	launch_cmd_if_file_in_list(t_token *tmp, char **path, char **cmd)
 	}
 }
 
-void	launch_cmd_without_file(char **path, char **options)
+/**
+ * This fonction launch the cmd if there is no file in the list. The i variable
+ * is a counter for the execve fonction and not created here for the norm.
+ * @param path 
+ * @param options 
+ * @param i 
+ * 
+ * @return void
+ */
+void	launch_cmd_without_file(char **path, char **options, int i)
 {
 	pid_t	pid;
 	int		status;
-	int		i;
 
 	i = -1;
 	pid = fork();
@@ -163,17 +79,29 @@ void	launch_cmd_without_file(char **path, char **options)
 		waitpid(pid, &status, 0);
 }
 
+/**
+ * This is the principal fonction in this file. It create a double char
+ * **cmd_with_options (file:launch_cmd_bis.c) with the cmd and options. 
+ * The path is the env variable with the cmd in the end (file:launch_cmd_bis.c).
+ * Then we check the list if there is a file in it or not (file:launch_cmd_bis.c)
+ * and we use the good fonction
+ * @param env 
+ * 
+ * @return void
+ */
 void	launch_cmd(t_env env)
 {
 	t_token	*tmp;
 	char	**cmd_with_options;
 	char	**path;
+	int		i;
 
+	i = 0;
 	tmp = env.token;
 	cmd_with_options = tab_with_cmd_and_options(tmp);
 	path = ft_create_path_for_execve(env.envp, cmd_with_options[0]);
 	if (search_e_file_in_list(tmp) == 1)
-		launch_cmd_if_file_in_list(tmp, path, cmd_with_options);
+		launch_cmd_if_file_in_list(tmp, path, cmd_with_options, i);
 	else
-		launch_cmd_without_file(path, cmd_with_options);
+		launch_cmd_without_file(path, cmd_with_options, i);
 }
---
+++ srcs/builtins/launch_cmd_bis.c
@@ -0,0 +1,130 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   launch_cmd_bis.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nleggeri <nleggeri@42.student.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/12 11:25:41 by nleggeri          #+#    #+#             */
+/*   Updated: 2023/05/12 11:28:36 by nleggeri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../minishell.h"
+
+/**
+ * This fonction search in the list if the first element is a cmd or not
+ * @param env
+ * 
+ * @return int
+ */
+int	check_if_cmd_first(t_env env)
+{
+	if (env.token->type == E_CMD)
+		return (1);
+	return (0);
+}
+
+/**
+ * Calcule how many size is the char **tab with E_CMD and E_SPACE and E_STRING
+ * @param token
+ * 
+ * @return int
+ */
+int	size_of_char_for_cmd_options(t_token *token)
+{
+	t_token	*tmp;
+	int		i;
+
+	i = 0;
+	tmp = token;
+	while (tmp)
+	{
+		if (tmp->type == E_CMD || tmp->str[0] == '-' || tmp->type == E_SPACE)
+		{
+			if (tmp->type != E_SPACE)
+				i++;
+			tmp = tmp->next;
+		}
+		else
+			break ;
+	}
+	return (i);
+}
+
+/**
+ * This fonction create an array with cmd and options for execve
+ * @param token
+ * 
+ * @return char**
+ */
+char	**tab_with_cmd_and_options(t_token *token)
+{
+	t_token	*tmp;
+	char	**tab;
+	int		i;
+
+	i = 0;
+	tab = malloc(sizeof(char *) * (size_of_char_for_cmd_options(token) + 1));
+	if (!tab)
+		return (NULL);
+	tmp = token;
+	while (i < size_of_char_for_cmd_options(token))
+	{
+		if (tmp->type == E_CMD || tmp->str[0] == '-')
+			tab[i++] = ft_strdup(tmp->str);
+		tmp = tmp->next;
+	}
+	tab[i] = NULL;
+	return (tab);
+}
+
+/**
+ * This fonction create the correct path who will be used in execve
+ * @param env 
+ * @param cmd
+ * 
+ * @return char** 
+ */
+char	**ft_create_path_for_execve(t_envp *env, char *cmd)
+{
+	t_envp	*tmp;
+	char	*env_path;
+	char	**envp;
+	int		i;
+
+	i = 0;
+	tmp = env;
+	while (tmp && ft_strncmp(tmp->name, "PATH", 4) != 0)
+		tmp = tmp->next;
+	env_path = ft_strdup(tmp->value);
+	envp = ft_split(env_path, ':');
+	while (envp[i])
+	{
+		envp[i] = join_all_path(envp[i], cmd, '/');
+		i++;
+	}
+	return (envp);
+}
+
+/**
+ * It search an E_FILE in the list and return 1 if it find it. It use in 
+ * launch_cmd fonction and separate the exec if there is a file in the list
+ * or not
+ * @param token
+ * 
+ * @return int 
+ */
+int	search_e_file_in_list(t_token *token)
+{
+	t_token	*tmp;
+
+	tmp = token;
+	while (tmp)
+	{
+		if (tmp->type == E_FILE)
+			return (1);
+		tmp = tmp->next;
+	}
+	return (0);
+}
